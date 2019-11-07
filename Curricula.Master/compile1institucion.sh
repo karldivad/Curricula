cf6d549b1b8aebb9a253615c81505e21e08b4e7b
Ernesto Cuadros-Vargas <ecuadros@gmail.com>
2019-11-07 15:35:30 -0500

    2020-I (V0.1)

--- Curricula.Master/compile1institucion.sh
+++ Curricula.Master/compile1institucion.sh
@@ -24,17 +24,17 @@ endif
 echo "pdf=$pdf, html=$html"
 
 set LogDir=../Curricula.out/log
-date > ../Curricula.out/log/Peru-IS-UTEC-time.txt
+date > ../Curricula.out/log/Peru-CS-UTEC-time.txt
 #--BEGIN-FILTERS--
 set institution=UTEC
 setenv CC_Institution UTEC
-set filter=SPC,UTEC
-setenv CC_Filter SPC,UTEC
+set filter=UTEC
+setenv CC_Filter UTEC
 set version=final
 setenv CC_Version final
-set area=IS
-setenv CC_Area IS
-set CurriculaParam=IS-UTEC
+set area=CS
+setenv CC_Area CS
+set CurriculaParam=CS-UTEC
 #--END-FILTERS--
 set curriculamain=curricula-main
 setenv CC_Main curricula-main
@@ -42,10 +42,10 @@ set current_dir = `pwd`
 
 set Country=Peru
 set OutputDir=../Curricula.out
-set OutputInstDir=../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020
-set OutputTexDir=../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/tex
-set OutputScriptsDir=../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts
-set OutputHtmlDir=../Curricula.out/html/Peru/IS-UTEC/Plan2020
+set OutputInstDir=../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018
+set OutputTexDir=../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/tex
+set OutputScriptsDir=../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts
+set OutputHtmlDir=../Curricula.out/html/Peru/CS-UTEC/Plan2018
 
 rm *.ps *.pdf *.log *.dvi *.aux *.bbl *.blg *.toc *.out *.xref *.lof *.log *.lot *.brf *~ *.tmp
 # ls IS*.tex | xargs -0 perl -pi -e 's/CATORCE/UNOCUATRO/g'
@@ -54,10 +54,10 @@ rm *.ps *.pdf *.log *.dvi *.aux *.bbl *.blg *.toc *.out *.xref *.lof *.log *.lot
 #sudo chown -R ecuadros:curricula ./Curricula
 
 mkdir -p ../Curricula.out/log
-./scripts/process-curricula.pl IS-UTEC ;
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-eps-files.sh;
-foreach lang ('ES')
-    ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-graph.sh small $lang
+./scripts/process-curricula.pl CS-UTEC ;
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-eps-files.sh;
+foreach lang ('ES' 'EN')
+    ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-graph.sh small $lang
 end
 
 if($pdf == 1) then
@@ -67,38 +67,38 @@ if($pdf == 1) then
       #bibtex curricula-main1;
 
       mkdir -p ../Curricula.out/log
-      ./scripts/compbib.sh curricula-main > ../Curricula.out/log/Peru-IS-UTEC-errors-bib.txt;
+      ./scripts/compbib.sh curricula-main > ../Curricula.out/log/Peru-CS-UTEC-errors-bib.txt;
 
       latex curricula-main;
       latex curricula-main;
 
-      echo IS-UTEC;
-      dvips curricula-main.dvi -o IS-UTEC.ps;
-      echo IS-UTEC;
-      ps2pdf IS-UTEC.ps IS-UTEC.pdf;
-      rm -rf IS-UTEC.ps;
+      echo CS-UTEC;
+      dvips curricula-main.dvi -o CS-UTEC.ps;
+      echo CS-UTEC;
+      ps2pdf CS-UTEC.ps CS-UTEC.pdf;
+      rm -rf CS-UTEC.ps;
 
 #     Generate the first page and place it at html dir
-      mutool convert -o ../Curricula.out/html/Peru/IS-UTEC/Plan2020/CurriculaMain-P1.png IS-UTEC.pdf 1-1
-      #pdftk A=IS-UTEC.pdf cat A1-1 output IS-UTEC-P1.pdf;
-      #convert IS-UTEC-P1.pdf IS-UTEC-P1.png;
-      #rm IS-UTEC-P1.pdf;
-      #mv IS-UTEC-P1.png ../Curricula.out/html/Peru/IS-UTEC/Plan2020/CurriculaMain-P1.png;
-      cp IS-UTEC.pdf ../Curricula.out/html/Peru/IS-UTEC/Plan2020/CurriculaMain.pdf;
-      mkdir -p "../Curricula.out/pdfs/IS-UTEC/Plan2020"
-      mv IS-UTEC.pdf "../Curricula.out/pdfs/IS-UTEC/Plan2020/IS-UTEC Plan2020.pdf";
+      mutool convert -o ../Curricula.out/html/Peru/CS-UTEC/Plan2018/CurriculaMain-P1.png CS-UTEC.pdf 1-1
+      #pdftk A=CS-UTEC.pdf cat A1-1 output CS-UTEC-P1.pdf;
+      #convert CS-UTEC-P1.pdf CS-UTEC-P1.png;
+      #rm CS-UTEC-P1.pdf;
+      #mv CS-UTEC-P1.png ../Curricula.out/html/Peru/CS-UTEC/Plan2018/CurriculaMain-P1.png;
+      cp CS-UTEC.pdf ../Curricula.out/html/Peru/CS-UTEC/Plan2018/CurriculaMain.pdf;
+      mkdir -p "../Curricula.out/pdfs/CS-UTEC/Plan2018"
+      mv CS-UTEC.pdf "../Curricula.out/pdfs/CS-UTEC/Plan2018/CS-UTEC Plan2018.pdf";
 endif
 
-./scripts/update-outcome-itemizes.pl IS-UTEC
-./scripts/update-page-numbers.pl IS-UTEC;
-foreach lang ('ES')
-    ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-graph.sh big $lang
+./scripts/update-outcome-itemizes.pl CS-UTEC
+./scripts/update-page-numbers.pl CS-UTEC;
+foreach lang ('ES' 'EN')
+    ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-graph.sh big $lang
 end
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-map-for-course.sh
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-map-for-course.sh
 
 if($html == 1) then
       rm unified-curricula-main* ;
-      ./scripts/gen-html-main.pl IS-UTEC;
+      ./scripts/gen-html-main.pl CS-UTEC;
       cp ../Curricula.in/css/curricula-main.css unified-curricula-main.css
 
       latex unified-curricula-main;
@@ -110,56 +110,56 @@ if($html == 1) then
       ps2pdf unified-curricula-main.ps unified-curricula-main.pdf;
       rm unified-curricula-main.ps unified-curricula-main.dvi;
 
-      rm -rf ../Curricula.out/html/Peru/IS-UTEC/Plan2020;
-      mkdir -p ../Curricula.out/html/Peru/IS-UTEC/Plan2020/figs;
-      cp ../Curricula.in/lang/Espanol/figs/pdf.jpeg ../Curricula.in/lang/Espanol/figs/star.gif ../Curricula.in/lang/Espanol/figs/none.gif ../Curricula.in/lang/Espanol/figs/*.png ../Curricula.out/html/Peru/IS-UTEC/Plan2020/figs/.;
+      rm -rf ../Curricula.out/html/Peru/CS-UTEC/Plan2018;
+      mkdir -p ../Curricula.out/html/Peru/CS-UTEC/Plan2018/figs;
+      cp ../Curricula.in/lang/Espanol/figs/pdf.jpeg ../Curricula.in/lang/Espanol/figs/star.gif ../Curricula.in/lang/Espanol/figs/none.gif ../Curricula.in/lang/Espanol/figs/*.png ../Curricula.out/html/Peru/CS-UTEC/Plan2018/figs/.;
 
-      latex2html -t "Curricula IS-UTEC" \
-      -dir "../Curricula.out/html/Peru/IS-UTEC/Plan2020/" -mkdir \
+      latex2html -t "Curricula CS-UTEC" \
+      -dir "../Curricula.out/html/Peru/CS-UTEC/Plan2018/" -mkdir \
       -toc_stars -local_icons -no_footnode -show_section_numbers -long_title 5 \
       -address "Generado por <A HREF='http://socios.spc.org.pe/ecuadros/'>Ernesto Cuadros-Vargas</A> <ecuadros AT spc.org.pe>,               <A HREF='http://www.spc.org.pe/'>Sociedad Peruana de Computaci&oacute;n-Peru</A>,               <A HREF='http://www.utec.edu.pe/'>Universidad de Ingenier&iacute;a y Tecnolog&iacute;a, Lima-Per&uacute;</A><BR>              basado en el modelo de la Computing Curricula de               <A HREF='http://www.computer.org/'>IEEE-CS</A>/<A HREF='http://www.acm.org/'>ACM</A>" \
       -white unified-curricula-main;
-      cp "../Curricula.out/html/Peru/IS-UTEC/Plan2020/Curricula_IS_UTEC.html" "../Curricula.out/html/Peru/IS-UTEC/Plan2020/index.html";
+      cp "../Curricula.out/html/Peru/CS-UTEC/Plan2018/Curricula_CS_UTEC.html" "../Curricula.out/html/Peru/CS-UTEC/Plan2018/index.html";
       #-split 3 -numbered_footnotes -images_only -timing -html_version latin1 -antialias -no_transparent \
 
 
-      ./scripts/update-analytic-info.pl IS-UTEC
-      ./scripts/gen-faculty-info.pl IS-UTEC
+      ./scripts/update-analytic-info.pl CS-UTEC
+      ./scripts/gen-faculty-info.pl CS-UTEC
 endif
 
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/compile-simple-latex.sh small-graph-curricula IS-UTEC-small-graph-curricula ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/tex;
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/compile-simple-latex.sh small-graph-curricula CS-UTEC-small-graph-curricula ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/tex;
 
-foreach lang ('ES')
-    ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-poster.sh $lang
+foreach lang ('ES' 'EN')
+    ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-poster.sh $lang
 end
 
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-syllabi.sh all;
-mkdir -p ../Curricula.out/html/Peru/IS-UTEC/Plan2020/syllabi;
-cp ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/syllabi/* ../Curricula.out/html/Peru/IS-UTEC/Plan2020/syllabi/.;
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-syllabi.sh all;
+mkdir -p ../Curricula.out/html/Peru/CS-UTEC/Plan2018/syllabi;
+cp ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/syllabi/* ../Curricula.out/html/Peru/CS-UTEC/Plan2018/syllabi/.;
 
 # Generate Books
 #
-# foreach auxbook (../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/tex/BookOf*-*.tex)
+# foreach auxbook (../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/tex/BookOf*-*.tex)
 #    set book = `echo $auxbook | sed s/.tex//`
-#    $book = `echo $book | sed s|../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/tex/||`
+#    $book = `echo $book | sed s|../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/tex/||`
 #    echo $book
 #    #bibtex $auxfile
-#    ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  $book       	pdflatex "IS-UTEC 2020-I $book (Plan2020) 1-10";
+#    ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  $book       	pdflatex "CS-UTEC 2020-I $book (Plan2018) 1-10";
 # end
 
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfSyllabi-ES  	 pdflatex "IS-UTEC 2020-I BookOfSyllabi-ES (Plan2020) 1-10";
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfSyllabi-EN  	 pdflatex "IS-UTEC 2020-I BookOfSyllabi-EN (Plan2020) 1-10";
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfBibliography-ES  pdflatex "IS-UTEC 2020-I BookOfBibliography-ES (Plan2020) 1-10";
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfBibliography-EN  pdflatex "IS-UTEC 2020-I BookOfBibliography-EN (Plan2020) 1-10";
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfDescriptions-ES  pdflatex "IS-UTEC 2020-I BookOfDescriptions-ES (Plan2020) 1-10";
-../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfDescriptions-EN  pdflatex "IS-UTEC 2020-I BookOfDescriptions-EN (Plan2020) 1-10";
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfSyllabi-ES  	 pdflatex "CS-UTEC 2020-I BookOfSyllabi-ES (Plan2018) 1-10";
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfSyllabi-EN  	 pdflatex "CS-UTEC 2020-I BookOfSyllabi-EN (Plan2018) 1-10";
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfBibliography-ES  pdflatex "CS-UTEC 2020-I BookOfBibliography-ES (Plan2018) 1-10";
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfBibliography-EN  pdflatex "CS-UTEC 2020-I BookOfBibliography-EN (Plan2018) 1-10";
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfDescriptions-ES  pdflatex "CS-UTEC 2020-I BookOfDescriptions-ES (Plan2018) 1-10";
+../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfDescriptions-EN  pdflatex "CS-UTEC 2020-I BookOfDescriptions-EN (Plan2018) 1-10";
 
-#       ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfUnitsByCourse 	latex    "IS-UTEC 2020-I BookOfUnitsByCourse (Plan2020) 1-10";
-#       ../Curricula.out/Peru/IS-UTEC/cycle/2020-I/Plan2020/scripts/gen-book.sh  BookOfDeliveryControl  pdflatex "IS-UTEC 2020-I BookOfDeliveryControl (Plan2020) 1-10";
+#       ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfUnitsByCourse 	latex    "CS-UTEC 2020-I BookOfUnitsByCourse (Plan2018) 1-10";
+#       ../Curricula.out/Peru/CS-UTEC/cycle/2020-I/Plan2018/scripts/gen-book.sh  BookOfDeliveryControl  pdflatex "CS-UTEC 2020-I BookOfDeliveryControl (Plan2018) 1-10";
 
 
-date >> ../Curricula.out/log/Peru-IS-UTEC-time.txt;
-more ../Curricula.out/log/Peru-IS-UTEC-time.txt;
+date >> ../Curricula.out/log/Peru-CS-UTEC-time.txt;
+more ../Curricula.out/log/Peru-CS-UTEC-time.txt;
 #./scripts/testenv.pl
 beep;
 beep;
